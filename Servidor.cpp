5ff9bf67b8072042e017392062a69de7ad9f12d3
Juan Pablo CS <juanpacarrillo1@gmail.com>
2020-03-01 15:47:01 -0600

    update Servidor

--- Servidor.cpp
+++ Servidor.cpp
@@ -7,7 +7,7 @@ class Servidor{
 public:
     //variables
     WSADATA WSAData;//declaracion objeto WSADATA
-    SOCKET Servidor, cliente; //sockets a utilizar
+    SOCKET Server, cliente; //sockets a utilizar
     SOCKADDR_IN serverAddr, clientAddr; //objeto que tiene info del socket como una dir IP o num del puerto
     char buffer[1024];// almacena los datos ingresados
 
@@ -18,7 +18,7 @@ public:
         //1 parametro: especifica la familia de direcciones. ya sea (AF_INET para protocolo ipv4) o (AF_INET6 para el ipv6)
         //2: especificacion de tipo. sock_stream abre una conexion entre dos pc distantes para que se cmuniquen
         //3: en 0, no se usara mucho, es el parametro de protocolo, que depende de la familia de direcciones y el tipo de socket usado antes
-        Servidor = socket(AF_INET, SOCK_STREAM, 0);
+        Server = socket(AF_INET, SOCK_STREAM, 0);
 
 //Una vez declarado el socket, se le indica a qué dirección IP debe conectarse y qué puerto debe usar:
 
@@ -32,20 +32,63 @@ public:
 
 //Luego se vincula el socket con la dirección IP y el número de puerto.
 //En cierto modo, es como la función connect () (los parámetros son los mismos) pero para el lado del servidor
-        bind(Servidor, (SOCKADDR *)&serverAddr, sizeof(serverAddr));
+        bind(Server, (SOCKADDR *)&serverAddr, sizeof(serverAddr));
         //Función que comienza a escuchar para permitir que los clientes se conecten. El primer parámetro es el servidor de socket, el segundo se deja en 0.
-        listen(server, 0);
-        cout << "Escuchando para conexiones entrantes." << endl;
+        listen(Server, 0);
+        cout << "Buscando conexiones entrantes." << endl;
         //variable para almacenar el tamaño de la dir del cliente
         int clientAddrSize = sizeof(clientAddr);
         //La función de aceptación permite un intento de conexión entrante en un socket.
-        if((cliente = accept(Servidor, (SOCKADDR *)&clientAddr, &clientAddrSize)) != INVALID_SOCKET)
+        if((cliente = accept(Server, (SOCKADDR *)&clientAddr, &clientAddrSize)) != INVALID_SOCKET)
         {
-            cout << "Cliente conectado!" << endl;
+            cout << "Cliente conectado!"<<endl;
         }
     }
 
+    //Funcion para Recibir mensajes del cliente
+    string Recibir()
+    {
+    //Funcion que recibe el mensaje del servidor tiene como parametros:
+    //El socket del servidor, Los datos (buffer), El tamaño de los datos, conjunto de banderas que se puede dejar en 0
+      recv(cliente, buffer, sizeof(buffer), 0);
+      cout << "El cliente dice: " << buffer << endl;
+      //Funcion que resetea el buffer a 0
+      memset(buffer, 0, sizeof(buffer));
+      //funcion para limpiar el buffer
+        fflush(stdin);
+    }
 
+    //Funcion para enviar mensajes al cliente
+    void Enviar()
+    {
+        cout<<"Escribe el mensaje a enviar: ";
+        cin>>this->buffer;
+        //Funcion que envia el mensaje al servidor tiene como parametros:
+        //El socket del servidor, Los datos (buffer), El tamaño de los datos, conjunto de banderas que se puede dejar en 0
+        send(cliente, buffer, sizeof(buffer), 0);
+        //funcion para limpiar el buffer
+        memset(buffer, 0, sizeof(buffer));
+        //funcion para limpiar el buffer
+        fflush(stdin);
+        cout << "Mensaje enviado!" << endl;
+    }
+    //Funcion para cerrar el socket
+    void CerrarSocket()
+    {
+      //Funcion que desconecta el socket del cliente
+        closesocket(cliente);
+        cout << "Socket cerrado, cliente desconectado." << endl;
+    }
 
 
 };
+
+int main()
+{
+  Servidor *servidor = new Servidor();
+  while(true)
+  {
+      servidor->Recibir();
+      servidor->Enviar();
+  }
+  }
